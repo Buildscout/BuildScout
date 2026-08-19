b0da5b7280a33ae1ff31a0e0fed4b3100b1b4c70
Buildscout <maven-tannins.5y@icloud.com>
2026-08-18 22:15:52 -0500

    Phase 16.2: mark DallasNow adapter version

---
+++ phase16-2-version.js
@@ -0,0 +1,6 @@
+window.BuildScoutPhase16_2 = Object.freeze({
+  version: "16.2",
+  name: "DallasNow current-source adapter",
+  productionFeedConnected: false,
+  integrityMode: "verified-source-only"
+});
