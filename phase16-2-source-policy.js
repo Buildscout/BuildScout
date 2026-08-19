3c721c31449b1c7bd426ed354e0a98d2ad5aaf15
Buildscout <maven-tannins.5y@icloud.com>
2026-08-18 22:14:13 -0500

    Phase 16.2: add production source policy

---
+++ phase16-2-source-policy.js
@@ -0,0 +1,9 @@
+window.BuildScoutSourcePolicy = Object.freeze({
+  requireOfficialSource: true,
+  requireRecordNumber: true,
+  requireProjectAddress: true,
+  requireSourceUrl: true,
+  allowFabricatedProductionRecords: false,
+  allowHistoricalAsCurrent: false,
+  allowUnauthorizedPortalScraping: false
+});
